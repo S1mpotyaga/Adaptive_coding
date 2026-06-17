ed9435435c216020c0f184665f5f2973cbed0a13
root <root@zfk.localdomain>
2026-06-17 17:20:46 +0300

    a little fix

--- cpp/src/data/adaptive_config.hpp
+++ cpp/src/data/adaptive_config.hpp
@@ -5,7 +5,7 @@ namespace adaptive{
 struct AdaptiveConfig{
     double initial_step = 1.0;
     double min_step = 0.1;
-    double max_step = 100.0;
+    double max_step = 255.0;
     double grow_factor = 1.2;
     double decay_factor = 0.8;
 };
